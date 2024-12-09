6decf0b086632b88b01274b2087817df28bdad9d
João Victor Borges Tavares <110401685+joaovictordevcod@users.noreply.github.com>
2024-12-09 20:55:53 -0300

    Update Cliente.java

--- class.java/Cliente.java
+++ class.java/Cliente.java
@@ -5,7 +5,6 @@ public class Cliente {
         Scanner scanner = new Scanner(System.in);
         TravelFacade travelFacade = new TravelFacade();
 
-        // Recebe informações do usuário para a reserva do voo
         System.out.println("Digite a cidade de origem do voo:");
         String origem = scanner.nextLine();
 
@@ -15,7 +14,6 @@ public class Cliente {
         System.out.println("Digite a data do voo (YYYY-MM-DD):");
         String dataVoo = scanner.nextLine();
 
-        // Recebe informações do usuário para a reserva do hotel
         System.out.println("Digite a cidade do hotel:");
         String cidadeHotel = scanner.nextLine();
 
@@ -25,16 +23,14 @@ public class Cliente {
         System.out.println("Digite a data de check-out do hotel (dia-mes-ano):");
         String dataCheckOut = scanner.nextLine();
 
-        // Recebe informações do usuário para o aluguel de carro
         System.out.println("Digite a data de início do aluguel de carro (dia-mes-ano):");
         String dataAluguelInicio = scanner.nextLine();
 
         System.out.println("Digite a data de fim do aluguel de carro (dia-mes-ano):");
         String dataAluguelFim = scanner.nextLine();
 
-        // Fazendo a reserva do pacote de viagem
         travelFacade.reservarPacote(origem, destino, cidadeHotel, dataVoo, dataCheckIn, dataCheckOut, dataAluguelInicio, dataAluguelFim);
 
-        scanner.close(); // Fechar o scanner ao final
+        scanner.close();
     }
 }
